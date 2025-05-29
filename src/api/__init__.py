790eef89f794d8cfdadac0baa2173c027cd62315
Mindcore M <tima.042007@mail.ru>
2025-05-29 17:32:10 +0600

    add initial project structure with FastAPI, PostgreSQL, and user management features

---
+++ src/api/__init__.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from src.api.users.views import router as router_users
+from src.core.config import settings
+
+router = APIRouter()
+
+router.include_router(router_users, prefix=settings.api.users.prefix)
+
+
+# Динамически импортируем все роутеры из папок
+# for path in Path(__file__).parent.glob("*/views.py"):
+#     module_name = f"api.{path.parent.name}.views"
+#     module = import_module(module_name)
+#     if hasattr(module, "router"):
+#         router.include_router(module.router, prefix=f"/{path.parent.name}")
---
+++ src/core/config.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = 8000
+
+class ApiUsersPrefix(BaseModel):
+    prefix: str = "/users"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    users: ApiUsersPrefix = ApiUsersPrefix()
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50  # кол-во соединений в пуле
+    max_overflow: int = 10  # кол-во дополнительных соединений
+    naming_convertion: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,  # чувствительность к регистрам
+        env_nested_delimiter="__",  # это деления как путь будет
+        env_prefix="APP_CONFIG__",
+        env_file=".env",
+    )
+    # вместо APP_CONFIG__ можно написать что угодно FASTAPI__ к примеру
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
+
+
+settings = Settings()
+print(settings.db.url)
